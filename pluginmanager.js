6cdf7c50b10044d0a5d3a9c50b584375922acb2e
Nick Vernij <hello@nickforall.nl>
2015-09-27 13:32:34 +0200

    Fixed bugs in, and cleaned pluginmanager.js
    - Cleaned humongous "for" loop
    - Larger plugins can now use folders
    - "All plugins loaded" is no longer fired when there aren't any

--- pluginmanager.js
+++ pluginmanager.js
@@ -1,6 +1,8 @@
 var fs = require("fs");
 
-var pluginlist;
+var pluginList;
+var pluginFolder;
+var reservedCommands;
 
 /**
  * Initializes the plugins folder, folder as given in the settings
@@ -9,32 +11,33 @@ var pluginlist;
  */
 exports.init = function(folder){
     try {
-        pluginList = fs.readdirSync(folder)
-        loadPlugins();
+        pluginList = fs.readdirSync(folder).filter(function(file) {
+            return fs.statSync(path.join(folder, file)).isFile();
+        });
+
+        pluginFolder = folder;
+        loadPlugins()
     }
     catch (err) {
         pluginList = false;
 
         print("Could not load contents of plugin folder", "red");
-        process.exit();
+        process.exit()
     }
-}
+};
 
 /**
- * Loads the plugns from the plugin list generated on initialization
+ * Loads the plugins from the plugin list generated on initialization
  */
 function loadPlugins(){
     if (pluginList) {
         if (pluginList.length == 0) {
             print("No plugins found", "red")
-        }
-        else {
-            var reservedCommands = {}
+        } else {
 
             //creates a listener for the bot emitter to check for illegal listeners
             bot.on("newListener", function(event, listener) {
-                var illegalListeners = ["newListener", "removeListener"]
-
+                var illegalListeners = ["newListener", "removeListener"];
                 if (illegalListeners.indexOf(event) != -1) {
                     setTimeout(function () {
                         bot.removeListener(event, listener)
@@ -42,58 +45,53 @@ function loadPlugins(){
 
                     print('Something tried to listen to the protected bot event "' + event + '"', "red")
                 }
-            })
+            });
 
-            for (var i = 0; i < pluginList.length; i++) {
+            loopThroughPluginsfolder(function (){
+                print("All plugins loaded");
+            });
+        }
+    }
+}
 
-                try {
-                    var plugin = require("./plugins/" + pluginList[i]).plugin
+function loopThroughPluginsfolder(callback){
+    reservedCommands = {};
 
-                    //does the plugin has a exports.plugins?
-                    if (!plugin) {
-                        print("Warning: plugin " + pluginList[i] + " does not implement exports.plugin", "red")
-                    }
-                    else {
-                        //do some fancy commands checking
-                        if (plugin.reservedCommands) {
-                            for (var t = 0; t < plugin.reservedCommands.length; t++) {
-                                if (reservedCommands[plugin.reservedCommands[t]]) {
-                                    print("Both " + pluginList[i] + " and " + reservedCommands[plugin.reservedCommands[t]] + " depend on the same command, please disable one of them.", "red")
-                                    process.exit();
-                                }
-                                else {
-                                    reservedCommands[plugin.reservedCommands[t]] = pluginList[i]
-                                }
-                            }
-                        }
-                        
-                        if (!plugin["name"]) {
-                            print("Loaded " + pluginList[i] + "!")
+    for (var i = 0; i < pluginList.length; i++) {
+        try {
+            var plugin = require("./" + pluginFolder + "/" + pluginList[i]).plugin;
+
+            //does the plugin has a exports.plugins?
+            if (!plugin) {
+                print("Warning: plugin " + pluginList[i] + " does not implement exports.plugin", "red")
+            } else {
+                //do some fancy commands checking
+                if (plugin.reservedCommands) {
+
+                    for (var t = 0; t < plugin.reservedCommands.length; t++) {
+                        if (reservedCommands[plugin.reservedCommands[t]]) {
+                            print("Both " + pluginList[i] + " and " + reservedCommands[plugin.reservedCommands[t]] + " depend on the same command, please disable one of them.", "red");
+                            process.exit();
                         }
                         else {
-                            print("Loaded " + plugin["name"] + " v" + plugin["version"] + "!")
+                            reservedCommands[plugin.reservedCommands[t]] = pluginList[i]
                         }
                     }
-                } catch (err) {
-                    print("Plugin " + pluginList[i] + " crashed with the following error:", "red")
-                    console.log(err.stack);
+
+                }
+
+                if (!plugin["name"]) {
+                    print("Loaded " + pluginList[i] + "!")
+                }
+                else {
+                    print("Loaded " + plugin["name"] + " v" + plugin["version"] + "!")
                 }
             }
+        } catch (err) {
+            print("Plugin " + pluginList[i] + " crashed with the following error:", "red");
+            console.log(err.stack);
         }
-
-        print("All plugins loaded")
-
-        // Security measures
-        bot.on("newListener", function(event, listener) {
-            var illegalListeners = ["newListener", "removeListener"]
-
-            if (illegalListeners.indexOf(event) != -1) {
-                setTimeout(function () {
-                    bot.removeListener(event, listener)
-                }, 10);
-
-                print('Something tried to listen to the protected bot event "' + event + '"', "red")
-            }
-        })
     }
+
+    callback()
 }
